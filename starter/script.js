8ed7f15c28f85eb2a209110e125ec03066197523
gbenga <odetoyinbog@gmail.com>
2025-01-15 14:34:52 +0100

    15th-3

--- starter/script.js
+++ starter/script.js
@@ -241,34 +241,76 @@ TEST DATA 2: Dolphins scored 85, 54, and 41. Koalas scored 23, 34, and 27.*/
 
 // console.log(bills, tips);
 
-const jonas = {
-  firstName: 'jonas',
-  lastName: 'screwtman',
-  age:2037 - 1991,
-  job: 'teacher',
-  friends: ['michael', 'peter', 'steven']
+// const jonas = {
+//   firstName: 'jonas',
+//   lastName: 'screwtman',
+//   age:2037 - 1991,
+//   job: 'teacher',
+//   friends: ['michael', 'peter', 'steven']
+// };
+// console.log(jonas.lastName);
+// console.log(jonas['lastName'])
+
+
+// const nameKey = 'Name';
+// console.log(jonas['first' + nameKey]);
+// console.log(jonas['last' + nameKey]);
+
+// const interestedIn = prompt('what do you want to know about jonas? choose betweeen firstName, lastName, age, job and friends');
+
+// if(jonas[interestedIn]) {
+//   console.log(jonas[interestedIn]);
+// }
+// else{
+//   console.log('wrong requwst! choose betweeen firstName, lastName, age, job and friends')
+// }
+
+// jonas.location = 'portugal';
+// jonas['twitter'] = '@jonalshmat';
+// console.log(jonas);
+
+// //challenge
+// //'jonas has 3 frieds, and his best friend is called michael'
+// console.log(`${jonas.firstName} has ${jonas.friends.length} friends and his best friend is called ${jonas.friends[0]}`);
+
+
+  const jonas = {
+    firstName: 'Jonas',
+    lastName: 'Schmedtmann',
+    birthYeah: 1991,
+    job: 'teacher',
+    friends: ['Michael', 'Peter', 'Steven'],
+    hasDriversLicense: false,
+
+  // calcAge: function(birthyeah){
+  //   return 2037 - this.birthYeah
+  // }
+
+//   calcAge: function(){
+//     console.log(this);
+//     return 2037 - this.birthYeah
+//   }
+// };
+
+calcAge: function(){
+  this.age = 2037 - this.birthYeah;
+  return this.age;
+},
+getSummary: function () {
+  return `${this.firstName} is a ${this.calcAge()}-year old ${jonas.job}, and he has ${this.hasDriversLicense ? 'a' : 'no'} driver's license.`
+}
 };
-console.log(jonas.lastName);
-console.log(jonas['lastName'])
+
+console.log(jonas.calcAge());
+
+console.log(jonas.age);
+console.log(jonas.age);
+console.log(jonas.age);
 
 
-const nameKey = 'Name';
-console.log(jonas['first' + nameKey]);
-console.log(jonas['last' + nameKey]);
-
-const interestedIn = prompt('what do you want to know about jonas? choose betweeen firstName, lastName, age, job and friends');
-
-if(jonas[interestedIn]) {
-  console.log(jonas[interestedIn]);
-}
-else{
-  console.log('wrong requwst! choose betweeen firstName, lastName, age, job and friends')
-}
-
-jonas.location = 'portugal';
-jonas['twitter'] = '@jonalshmat';
-console.log(jonas);
+// console.log(jonas['calcAge'] (1991));
 
 //challenge
-//'jonas has 3 frieds, and his best friend is called michael'
-console.log(`${jonas.firstName} has ${jonas.friends.length} friends and his best friend is called ${jonas.friends[0]}`);
+//jonas if a 46 year old teacher and he has a drivers license
+
+console.log(jonas.getSummary())
