f0f2a9ebef048184915408b313e91df4ad705e83
gbenga <odetoyinbog@gmail.com>
2025-01-15 10:57:55 +0100

    15th

--- starter/script.js
+++ starter/script.js
@@ -186,6 +186,57 @@ TEST DATA 2: Dolphins scored 85, 54, and 41. Koalas scored 23, 34, and 27.*/
 // const age3 = calcAge(years[years.length - 1]);
 // console.log(age1, age2, age3)
 
-const friend1 = 'Michael';
-const friend2 = 'Steven';
-const friend3 = 'Peter';
+// const friends = ['Michael', 'Steven', 'Peter']
+// //add elements
+// const newLength = friends.push('jay')
+// console.log(friends);
+// console.log(newLength);
+
+// friends.unshift('john');
+// console.log(friends);
+
+// //remove elements
+// friends.pop()
+// const popped = friends.pop();
+// console.log(popped);
+// console.log(friends);
+
+// friends.shift();
+// console.log(friends);
+// console.log(friends.indexOf('Steven'));
+
+
+// friends.push(23);
+// console.log(friends.includes('Steven'))
+// console.log(friends.includes('bob'));
+// console.log(friends.includes(23));
+
+// if (friends.includes('peter'));{
+//   console.log('you have a friend called peter')
+// }
+
+// CHALLENGE #2
+// Steven wants you to improve his tip calculator, using the same rules as before — tip 15% of the bill if the bill value is between 50 and 300, and if the value is different, the tip is 20%.
+
+// Your tasks:
+
+// Write a function calcTip that takes any bill value as an input and returns the corresponding tip, calculated based on the rules above (you can check out the code from the first tip calculator challenge if you need to). Use the function type you like the most. Test the function using a bill value of 100.
+
+// And now let's use arrays! So, create an array called bills containing the test data below.
+
+// Create an array called tips containing the tip value for each bill, calculated from the function you created before.
+
+// BONUS: Create an array totals containing the total values, so the bill + tip.
+
+// TEST DATA: 125, 555, and 44.
+const calcTip = function(bill){
+  return bill >= 50 && bill <=300 ? bill * 0.15: bill * 0.2;
+}
+
+// const calcTip = bill => bill >= 50 && <=300 ? bill * 0.15: bill * 0.2;
+
+const bills = [125, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2] ];
+
+console.log(bills, tips);
