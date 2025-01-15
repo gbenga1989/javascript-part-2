ab53a780827067167f889a3814ceace92911a45d
gbenga <odetoyinbog@gmail.com>
2025-01-15 16:31:09 +0100

    15th-5

--- starter/script.js
+++ starter/script.js
@@ -327,37 +327,52 @@ TEST DATA: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.9
 GOOD LUCK 😀
 */
 
-const mark = {
-  fullName: 'Mark Miller',
-  mass: 78,
-  height: 1.69,
-  calcBMI: function(){
-    this.bmi = this.mass / this.height ** 2;
-    return this.bmi;
-  }
+// const mark = {
+//   fullName: 'Mark Miller',
+//   mass: 78,
+//   height: 1.69,
+//   calcBMI: function(){
+//     this.bmi = this.mass / this.height ** 2;
+//     return this.bmi;
+//   }
   
+// }
+
+// const john = {
+//   fullName: 'John Smith',
+//   mass: 92,
+//   height: 1.95,
+//   calcBMI: function(){
+//     this.bmi = this.mass / this.height ** 2;
+//     return this.bmi;
+// }
+// }
+
+
+// mark.calcBMI();
+// john.calcBMI();
+// console.log(mark.bmi, john.bmi);
+
+// // John Smith's BMI (28.3) is higher than Mark Miller's (23.9)
+
+// if(mark.bmi > john.bmi){
+//   console.log(`${mark.fullName}'s BMI (${mark.bmi}) is higher than ${john.fullName}'s BMI(${john.bmi})`)
+// } else if (mark.bmi > john.bmi){
+//   console.log(`${john.fullName}'s BMI (${john.bmi}) is higher than ${mark.fullname}'s BMI(${mark.bmi})`)
+// }
+
+// console.log('lifting weigts repetition 1');
+// console.log('lifting weigts repetition 2');
+// console.log('lifting weigts repetition 3');
+// console.log('lifting weigts repetition 4');
+// console.log('lifting weigts repetition 5');
+// console.log('lifting weigts repetition 6');
+// console.log('lifting weigts repetition 7');
+// console.log('lifting weigts repetition 8');
+// console.log('lifting weigts repetition 9');
+// console.log('lifting weigts repetition 10');
+
+// for loop keeps running while condition is true
+for(let rep = 1; rep <= 10; rep++){
+  console.log(`lifting weigts repetition ${rep}`);
 }
-
-const john = {
-  fullName: 'John Smith',
-  mass: 92,
-  height: 1.95,
-  calcBMI: function(){
-    this.bmi = this.mass / this.height ** 2;
-    return this.bmi;
-}
-}
-
-
-mark.calcBMI();
-john.calcBMI();
-console.log(mark.bmi, john.bmi);
-
-// John Smith's BMI (28.3) is higher than Mark Miller's (23.9)
-
-if(mark.bmi > john.bmi){
-  console.log(`${mark.fullName}'s BMI (${mark.bmi}) is higher than ${john.fullName}'s BMI(${john.bmi})`)
-} else if (mark.bmi > john.bmi){
-  console.log(`${john.fullName}'s BMI (${john.bmi}) is higher than ${mark.fullname}'s BMI(${mark.bmi})`)
-}
-
